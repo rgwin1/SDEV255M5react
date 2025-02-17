ee00cf2046dcf62834c79a0fcf81a8531d433d6a
Ryan <rgwin1@ivytech.edu>
2025-02-17 05:21:33 -0500

    .

--- react/vite.config.js
+++ react/vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/SDEV255M5React/", 
+  base: "/SDEV255M5React/React", 
   plugins: [react()],
 })
